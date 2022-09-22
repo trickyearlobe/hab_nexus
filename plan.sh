cd88819c57fae4d60b14989054bd10b2a68c3fc8
Richard Nixon <richard.nixon@btinternet>
2022-09-22 17:51:22 +0100

    Fix docker export
    Nexus has a nexus.vmoptions file in the package bin directory which is owned by root and cannot be updated by the service
    at runtime because it runs as the hab user. The only thing we actually need to modify in that file is the path to the Nexus
    work directory which is normally /hab/svc/nexus/sonatype-work.
    
    To overcome this we were creating a dangling symlink to a dynamically generated version of nexus.vmoptions in the
    /hab/svc/nexus/config directory which only exists at run time. While this worked just fine for a hab environment running
    directly in a VM, hab's container export function doesn't like it when we export to docker.
    
    The fix is to remove dangling symlink and hard code the paths in the nexus.vmoptions file owned by root.
    
    In reality, this should not be terrible, as it's still fairly easy to redirect the actual work directory using filesystem
    mounts, or symlinks.
    
    Signed-off-by: Richard Nixon <richard.nixon@btinternet>

--- plan.sh
+++ plan.sh
@@ -42,20 +42,14 @@ do_install() {
   echo Copying Nexus binaries from $HAB_CACHE_SRC_PATH/nexus-$pkg_fq_version to $pkg_prefix
   cp -RT $HAB_CACHE_SRC_PATH/nexus-$pkg_fq_version $pkg_prefix
 
-  # Save nexus.vmoptions in case we want to review it later
-  # We will replace it with a symlink to the dynamic config.
-  # We do it here because the run hook doesn't have sufficient rights.
-  echo Saving $pkg_prefix/bin/nexus.vmoptions
-  mv $pkg_prefix/bin/nexus.vmoptions $pkg_prefix/bin/nexus.vmoptions.example
-  echo Symlinking $pkg_svc_config_path/nexus.vmoptions to $pkg_prefix/bin/nexus.vmoptions
-  ln -s $pkg_svc_config_path/nexus.vmoptions $pkg_prefix/bin/nexus.vmoptions
-
-
   # Copy across the template sonatype-work directory.
   # We'll use this to construct the default repo under
   # /hab/svc which will persist across upgrades.
   echo Copying $HAB_CACHE_SRC_PATH/sonatype-work to $pkg_prefix
   cp -R $HAB_CACHE_SRC_PATH/sonatype-work $pkg_prefix
+
+  # Update nexus.vmoptions to use the service's data directory.
+  sed -i 's/..\/sonatype-work/\/hab\/svc\/nexus\/data\/sonatype-work/g' $pkg_prefix/bin/nexus.vmoptions
 
   return 0
 }
